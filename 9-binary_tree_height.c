ab430f7dbf8c86ce3c54b59121bb5fd4327126b3
the1Riddle <elvisotienomboya@gmail.com>
2023-11-01 03:06:54 -0700

    unfortunately betty was forgoten: betty errors solved

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - check if a binary tree node is a leaf node
+ * check_is_tree_leaf - check if a binary tree node is a leaf node
  * @node: pointer to the binary tree node to be checked.
  *
  * Return: 1 if the node is a leaf or 0 if not
@@ -18,7 +18,7 @@ int check_is_tree_leaf(const binary_tree_t *node)
  * binary_tree_height - a function that measures the height of a binary tree
  * @tree: a pointer to the root node of the tree to measure the height
  *
- * Return: 0 while tree is NULL	
+ * Return: 0 while tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
