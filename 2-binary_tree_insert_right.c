9f625211470371d61d1f80804cd1345d33d4c369
Heavenboi <nigelzenda98@gmail.com>
2023-11-01 14:09:49 +0200

    fixing issues with git

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,4 +1,4 @@
-#include "2-binary_tree_insert_right.c"
+#include "3-binary_tree_insert_right.c"
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
@@ -10,9 +10,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
     new = malloc(sizeof(binary_tree_t));
     if(new == NULL)
         return NULL;
-
-    new->n = value;
-    new->parent = parent;
     new->left = NULL;
     new->right = parent->right;
     parent->right = new;
