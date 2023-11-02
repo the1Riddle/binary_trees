4da503e01bf4d7b3d81b179b1a3cbdfdb6eb6fc3
the1Riddle <elvisotienomboya@gmail.com>
2023-11-02 01:53:31 -0700

    adding a block of code from file 9-b* to 0-b* to see if 13-b* will pass the checker

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,20 +1,5 @@
 #include "binary_trees.h"
 /**
- * check_is_tree_leaf - check if a binary tree node is a leaf node
- * @node: pointer to the binary tree node to be checked.
- *
- * Return: 1 if the node is a leaf or 0 if not
- */
-int check_is_tree_leaf(const binary_tree_t *node)
-{
-	if (node != NULL && !node->left && !node->right)
-	{
-		return (1);
-	}
-	return (0);
-}
-
-/**
  * binary_tree_height - a function that measures the height of a binary tree
  * @tree: a pointer to the root node of the tree to measure the height
  *
